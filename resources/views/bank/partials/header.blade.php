2f0ef1d97aa1c8010279535bede290bc444dd88e
mooosmad <mooosmad@gmail.com>
2024-02-09 17:30:28 +0000

    fix:images

--- resources/views/bank/partials/header.blade.php
+++ resources/views/bank/partials/header.blade.php
@@ -2,7 +2,7 @@
     <a href="/" class="brand-logo">
         <!-- <img class="logo-abbr" src="images/logo.png" alt=""> -->
         <img class="logo-compact" src="{{ asset('assets/images/credit.png') }}" alt="">
-        <img class="brand-title" src="{{ asset('BNP-Paribas-logo.png') }}" alt="">
+        <img class="brand-title" src="{{ asset('assets/images/credit.png') }}" alt="">
     </a>
 
     <div class="nav-control">
