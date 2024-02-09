8483a09c98949c09c17b6cfb5be59ec2be327335
mooosmad <mooosmad@gmail.com>
2024-02-09 16:44:53 +0000

    fix: finance

--- resources/views/livewire/transaction.blade.php
+++ resources/views/livewire/transaction.blade.php
@@ -50,10 +50,7 @@
 					<div class="card-body  p-4">
 						<div class="media ai-icon">
 							<span class="me-3 bgl-danger text-danger">
-								<svg id="icon-revenue" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign">
-									<line x1="12" y1="1" x2="12" y2="23"></line>
-									<path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-								</svg>
+								
 							</span>
 							<div class="media-body">
 								<p class="mb-1">Echouer</p>
